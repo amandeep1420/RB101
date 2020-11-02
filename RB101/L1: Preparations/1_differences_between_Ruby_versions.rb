3b3078b96a5fb3b1eaa710c93f0a9d5f4b33d5de
Aman Bal <amandeep1420@gmail.com>
2020-11-02 19:34:26 +0000

    Second commit.

---
+++ RB101/L1: Preparations/1_differences_between_Ruby_versions.rb
@@ -0,0 +1,4 @@
+# start checking your specific Ruby version (2.6.3) @ ruby-doc.org under "Core API" when looking for info.
+# be aware that things change from version to version - deprecations, cosolidations, additions, etc.
+# Launch School tends to link to Ruby definitions for the ~most recent version~ - keep this in mind.
+# in some situations, such as an assessment, you may occasionally be required to refer to a particular thing in line with a specific change to the Ruby API.
