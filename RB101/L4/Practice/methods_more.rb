b09bcd7ad269d12587220be7d9eb7f5d0925f0e7
amandeep1420 <amandeep1420@gmail.com>
2020-12-22 19:17:48 +0000

    Brb.

---
+++ RB101/L4/Practice/methods_more.rb
@@ -0,0 +1,9 @@
+1.
+
+an empty array - nil is the last expression in the block, so the block returns nil for every iteration - meaning no values from the
+collection are selected.
+wrong - puts wasn't used.' come on now. a string by itself is truthy, so the returned collection would've been the exact same as the original.' focus up.
+
+2. 
+
+looking at docs because I'm not familiar with #count...'
