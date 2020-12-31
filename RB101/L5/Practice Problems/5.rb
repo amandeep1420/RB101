e6153432c81f96c5ef629f69b8184d7df4b49eec
amandeep1420 <amandeep1420@gmail.com>
2020-12-31 00:56:34 +0000

    Don't give up.

---
+++ RB101/L5/Practice Problems/5.rb
@@ -0,0 +1,21 @@
+# munsters = {
+#   "Herman" => { "age" => 32, "gender" => "male" },
+#   "Lily" => { "age" => 30, "gender" => "female" },
+#   "Grandpa" => { "age" => 402, "gender" => "male" },
+#   "Eddie" => { "age" => 10, "gender" => "male" },
+#   "Marilyn" => { "age" => 23, "gender" => "female"}
+# }
+
+munsters1 = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female" }
+}
+
+total_age = 0
+munsters1.each do |_, traits|
+  total_age += traits["age"] if traits["gender"] == "male"
+end
+puts total_age
---
+++ RB101/L5/Practice Problems/7.rb
@@ -0,0 +1,12 @@
+a = 2
+b = [5, 8]
+arr = [a, b]
+
+arr[0] += 2
+arr[1][0] -= a
+
+# a is now 4
+# b is now [3, 8]
+
+puts a
+puts b
